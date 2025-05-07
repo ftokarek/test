465d4f8e46692740b197403ef6e592ace33aac94
otter <ke1241ks@gmail.com>
2025-05-07 03:44:22 +0200

    Model prmptu v 0.11

--- backend/prompt_model.py
+++ backend/prompt_model.py
@@ -2,15 +2,16 @@ from pydantic import BaseModel, Field
 from typing import List, Optional
 
 class PromptBase(BaseModel):
+    ##podstawa##
     text: str
-    chatbot_name: str
     owner_id: int
-    tags: Optional[List[str]] = Field(default_factory=list)
-    temperature: Optional[float] = 0.7
-    max_tokens: Optional[int] = 150
-    top_p: Optional[float] = 1.0
-    stop: Optional[List[str]] = None
-    model: Optional[str] = None #Uzupełnić później
+    chatbot_model: str
+    ###########
+    tags: Optional[List[str]] = Field(default_factory=list) #Kategoryzacja
+    temperature: Optional[float] = 0.7 #Do randomizacji (raczej useless)
+    max_tokens: Optional[int] = 150 #Tokeny
+    #top_p: Optional[float] = 1.0 #KOlejne randomizacje (też raczej useless)
+    stop: Optional[List[str]] = None #LIsta słów zatrzymujących generację
 
 class PromptCreate(PromptBase):
     pass
