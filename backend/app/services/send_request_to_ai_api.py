92c9c11c609911f9d267661f8235624ce46a1108
Filip Malejki <Filip.Malejki@ibm.com>
2025-05-16 22:20:10 +0200

    fix(backend/services): send req to ai api

--- backend/app/services/send_request_to_ai_api.py
+++ backend/app/services/send_request_to_ai_api.py
@@ -1,5 +1,7 @@
+import asyncio
 from typing import Dict, List, Any
 
+from app.models.prompt_model import PromptModel
 from app.services.openai_service import build_payload, parse_response
 
 from app.services.user_conversation_message_adder import add_message_to_conversation
@@ -9,25 +11,25 @@ from app.services.openai_service import send_to_openai, send_to_gemini, send_to_
 
 
 async def send_request_to_ai_api(
-    prompts_data: List[Dict[str, Any]],
+    prompts_data: List[PromptModel],
     model_info: str,
     user_message: str,
     user_id: str,
     conversation_id: str,
 ) -> Dict[str, Any]:
     try:
-
-        full_prompt = "\n\n".join([prompt["text"] for prompt in prompts_data]) + f"\n\n{user_message}"
-
+        print("inside send_request_to_ai_api")
+        full_prompt = "\n\n".join([prompt.text for prompt in prompts_data]) + f"\n\n{user_message}"
+        asyncio.create_task(add_message_to_conversation(conversation_id, user_message, "user"))
         # Wybierz odpowiednie API na podstawie api_choice
         if model_info == "openai":
             #response = send_to_openai(full_prompt)
-            response = send_to_gemini(full_prompt)
+            response = send_to_gemini(full_prompt, conversation_id, user_id)
         elif model_info == "gemini":
-            response = send_to_gemini(full_prompt)
+            response = send_to_gemini(full_prompt, conversation_id, user_id)
         elif model_info == "huggingface":
             #response = send_to_hugging_face(full_prompt)
-            response = send_to_gemini(full_prompt)
+            response = send_to_gemini(full_prompt, conversation_id, user_id)
         else:
             raise ValueError(f"Invalid API choice: {model_info}. Please choose 'openai', 'gemini', or 'huggingface'.")
 
