3e135d6624422ab575fe2875637fde027c222ccc
Filip Malejki <Filip.Malejki@ibm.com>
2025-05-16 22:20:51 +0200

    fix(backend/services): user_conversation_message_adder async added

--- backend/app/services/user_conversation_message_adder.py
+++ backend/app/services/user_conversation_message_adder.py
@@ -5,10 +5,11 @@ from app.db.get_conversation_info import get_conversation_info, update_conversat
 from typing import List
 from bson import ObjectId
 
-def add_message_to_conversation(conversation_id: str, message: str, rol: str):
+async def add_message_to_conversation(conversation_id: str, message: str, rol: str):
     #Adds a message to the conversation with the given ID.
     try:
-        update_message_to_conversation(conversation_id, message, rol)
+        print("before next step")
+        await update_message_to_conversation(conversation_id, message, rol)
     except Exception as e:
         raise RuntimeError(f"Error while adding message to conversation: {e}")
 
